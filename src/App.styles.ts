7a6f4aa89508c63629affed5f097c3bcaa6e7c9c
Stefanos <stefanosu422@gmail.com>
2020-11-01 20:23:43 -0500

    added img

--- src/App.styles.ts
+++ src/App.styles.ts
@@ -23,7 +23,7 @@ export const GlobalStyle = createGlobalStyle `
   }
 `
 
-export const Wrapper = styled.div `
+export const Wrapper = styled.div ` 
   display: flex; 
   flex-direction: column;
   align-items: center
