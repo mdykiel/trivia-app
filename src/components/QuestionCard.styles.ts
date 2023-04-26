57820d637e215335af17e0bfef881b969df4c1b4
mdykiel <miro24@gmail.com>
2023-04-26 15:50:06 +0200

    Update QuestionCard.styles.ts

--- src/components/QuestionCard.styles.ts
+++ src/components/QuestionCard.styles.ts
@@ -27,7 +27,7 @@ export const ButtonWrapper = styled.div<ButtonWrapperProps>`
     button {
         cursor: pointer;
         user-select: none;
-        font-size: 0.8rem;
+        font-size: 1.0 rem;
         width: 100%;
         height: 40px;
         margin: 5px 0;
